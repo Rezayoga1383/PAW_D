9080ed5764461f1d31d42d5ad32eda5fefb00815
Taufiqu Reza Yoga Pratama <102010153+Rezayoga1383@users.noreply.github.com>
2022-08-30 20:34:43 +0700

    Output Pertemuan Ke 2
    21-014_Taufiqu Reza Yoga Pratama

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+    function penjumlahan ($a , $b, $c){
+        $tambah = $a + $b + $c;
+        echo "<br> hasil penjumlahan dari ketiga bilangan tersebut adalah = ".$tambah;
+    }
+
+penjumlahan(10,2,3);
+penjumlahan(10,20,30);
+penjumlahan(50,10,20);
+?>
